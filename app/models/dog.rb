2cb5d7e84eb95f78f9b7eec8e9de65bb3d6e24f2
pierre dunoyer <dunoyerpierre@hotmail.fr>
2025-12-02 15:18:57 +0100

    in progress

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,16 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :tags, through: :dogs_tags
+
+
+  BREEDS = [
+    "Labrador",
+    "Berger Allemand",
+    "Bulldog",
+    "Beagle",
+    "Caniche",
+    "Setter",
+    "Boxer",
+    "Autre"
+  ].freeze
 end
