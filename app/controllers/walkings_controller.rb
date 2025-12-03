130168e9610dd772bd41a79f67eba00204d3c406
bricevaillant <naw.brice@gmail.com>
2025-12-03 15:36:40 +0100

    added walking#new page

--- app/controllers/walkings_controller.rb
+++ app/controllers/walkings_controller.rb
@@ -3,8 +3,37 @@ class WalkingsController < ApplicationController
   end
 
   def new
+    @walking = Walking.new
+  end
+
+  def create
+    @walking = Walking.new(walking_params)
+    
+    # need to generate les coordinates here
+    coordinates = [
+      "4.832319,45.756727",
+      "4.830339,45.759858",
+      "4.832083,45.763224",
+      "4.835149,45.762943",
+      "4.835395,45.757722",
+      "4.832319,45.756727"
+    ] # placeholder for actual coordinates generation logic
+
+    @circuit = Circuit.new(duration: @walking.wanted_duration, coordinates: coordinates, user: current_user)
+    raise
+    if @walking.save
+      redirect_to walkings_path(@walkings), notice: 'Walking was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def walking_params
+    params.require(:walking).permit(:wanted_duration, :sociable)
   end
 end
