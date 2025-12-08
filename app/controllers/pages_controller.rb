bb46e6a1ee9f13dc06ade37a4dff4a5103611559
pierre dunoyer <dunoyerpierre@hotmail.fr>
2025-12-08 17:15:02 +0100

    add-meetup-point-homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,22 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:isochrone]
+
   def home
+    @meetups = MeetUp.all
+
+    @markers = @meetups.map do |meetup|
+      latitude = Point.find(meetup.point_id).latitude
+      longitude = Point.find(meetup.point_id).longitude
+      {
+        lat: latitude,
+        lng: longitude,
+        # Optionnel : info window
+        #info_window_html: render_to_string(partial: "info_window", locals: { meetup: meetup }),
+        # Optionnel : image custom
+        #marker_html: render_to_string(partial: "marker", locals: { meetup: meetup })
+      }
+    end
   end
 
   def isochrone
