f1b54a9315b83a92aa1b3c33ab5fa1660ad25e2f
bricevaillant <naw.brice@gmail.com>
2025-12-09 17:00:27 +0100

    create walking data in seed for new user and acitivites page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  def static_map_url(walking)
+    return "" unless walking.coordinates.present?
+
+    coords = walking.coordinates
+    coords = JSON.parse(coords) if coords.is_a?(String)
+    return "" unless coords.is_a?(Array) && coords.any?
+
+    # if it's too big the api won't work
+    if coords.length > 20
+      step = (coords.length / 20).ceil
+      coords = coords.select.with_index { |_, i| i % step == 0 }
+    end
+
+    # this make a closed loop if not already
+    coords << coords.first if coords.first != coords.last
+
+    # that's what we send to mapbox api
+    geojson = {
+      type: "Feature",
+      properties: {
+        stroke: "#ff4500",
+        "stroke-width": 3,
+        "stroke-opacity": 0.8
+      },
+      geometry: {
+        type: "LineString",
+        coordinates: coords
+      }
+    }
+
+    encoded_geojson = CGI.escape(geojson.to_json)
+
+    "https://api.mapbox.com/styles/v1/mapbox/dark-v11/static/geojson(#{encoded_geojson})/auto/600x300?padding=60&access_token=#{ENV['MAPBOX_API_KEY']}"
+  end
 end
