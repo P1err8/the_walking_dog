1990f395d6479669c4b7c2f22620abc5f9d4f202
pierre dunoyer <dunoyerpierre@hotmail.fr>
2025-12-10 14:33:24 +0100

    removeRoundtrips commented

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import { cleanMapData, cleanMapDataFromElement } from "./utils/removeRoundtrips"
+// import { cleanMapData, cleanMapDataFromElement } from "./utils/removeRoundtrips"
 
 // Exposer les fonctions sur window pour pouvoir les appeler depuis le DOM
-window.cleanMapData = cleanMapData
-window.cleanMapDataFromElement = cleanMapDataFromElement
+// window.cleanMapData = cleanMapData
+// window.cleanMapDataFromElement = cleanMapDataFromElement
