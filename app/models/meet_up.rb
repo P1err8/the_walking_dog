9d28e681b3d93281cca4bbc04e29742607004749
bricevaillant <naw.brice@gmail.com>
2025-12-10 15:16:53 +0100

    creation function qui permet de savoir si le meetup est actif

--- app/models/meet_up.rb
+++ app/models/meet_up.rb
@@ -3,4 +3,18 @@ class MeetUp < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :users, through: :participations
   has_many :dogs, through: :users
+
+  scope :active, -> { joins(:participations).where('participations.created_at > ?', 1.hours.ago).distinct }
+
+   def active?
+    # Get the time of the last participation, or the meetup creation time if no one joined yet
+    last_activity_time = participations.maximum(:created_at) || created_at
+
+    # The meetup is active if the last activity was less than 2 hours ago
+    last_activity_time > 1.hours.ago
+  end
+
+  def closed?
+    !active?
+  end
 end
--- app/models/point.rb
+++ app/models/point.rb
@@ -1,3 +1,4 @@
 class Point < ApplicationRecord
   has_many :meet_ups, dependent: :destroy
+  
 end
